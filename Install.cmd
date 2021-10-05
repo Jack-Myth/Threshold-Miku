17e7c9c2bd30e60546bca1b6b84e6f36167aef90
JackMyth <wwwbkkk@126.com>
2021-10-06 03:08:29 +0800

    Upgrade TMC2.exe

--- Install.cmd
+++ Install.cmd
@@ -1,18 +1,29 @@
+@echo off
 FOR /F "tokens=2* skip=2" %%a in ('reg query "HKCU\Software\Valve\Steam" /v "SteamPath"') do set SteamPath=%%b
 REM If return value is not an exist path(Err Msg)
 if not exist "%SteamPath%" (
-    echo " >No steam detected. Please run steam at least once."
+    echo No steam detected. Please run steam at least once.
     pause
     exit
 )
 for %%I in (.) do set CurDirName=%%~nxI
 if exist "%SteamPath%\skins\%CurDirName%" (
-    set /p Override=!^> Skin already existed, Override?[y/N]
-    if %Override%==y (
-    if %Override%==Y (
-        del /f "%SteamPath%\skins\%CurDirName%"
-        copy "..\%CurDirName%" "%SteamPath%\skins\%CurDirName%"
-        reg add HKCU\Software\Valve\Steam /v SkinV5 /t REG_SZ /d %CurDirName% /f
-        echo " > Enjoy your new steam! :^)"
-    ))
+    goto Override
+) else (
+    goto Apply
 )
+exit
+
+:Override
+set /P Override=Skin already existed, Override?[y/N]
+if "%Override%"=="y" goto Apply 
+if "%Override%"=="Y" goto Apply
+exit
+
+:Apply
+del /f "%SteamPath%\skins\%CurDirName%"
+mkdir "%SteamPath%\skins"
+xcopy /e "..\%CurDirName%\" "%SteamPath%\skins\%CurDirName%\"
+reg add HKCU\Software\Valve\Steam /v SkinV5 /t REG_SZ /d %CurDirName% /f
+echo Enjoy your new steam! :^)
+start explorer.exe "%SteamPath%\skins\%CurDirName%"
